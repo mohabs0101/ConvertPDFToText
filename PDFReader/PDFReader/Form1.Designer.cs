e94dd442f19660523c96eed34b98cd92e797b642
mohammed abbas <72379773+mohabs0101@users.noreply.github.com>
2022-10-08 14:34:26 +0300

    update

---
+++ PDFReader/PDFReader/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PDFReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timPrint = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(784, 12);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(766, 518);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(784, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Print";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timPrint
+            // 
+            this.timPrint.Tick += new System.EventHandler(this.timPrint_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 551);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnConvert);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timPrint;
+    }
+}
+
---
+++ PDFReader/PDFReader/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using PDFReader;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+using System.Diagnostics;
+using System.Drawing.Printing;
+
+namespace PDFReader
+{
+    public partial class Form1 : Form
+    {
+        PrintDocument document = new PrintDocument();
+        //PrintDialog dialog = new PrintDialog();
+        string path = @"c:\\14.pdf";
+        string str = "";
+        public Form1()
+        {
+            InitializeComponent();
+            timPrint.Interval = (60 * 1000);
+            timPrint.Tick += new EventHandler(timPrint_Tick);
+            timPrint.Start();
+            document.PrintPage += new PrintPageEventHandler(document_PrintPage);
+        }
+        void document_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            float leftMargin = e.MarginBounds.Left;
+            e.Graphics.DrawString(str, new Font("Arial", 10, FontStyle.Regular), Brushes.Black, 15, 50);
+        }
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            PDDocument p = PDDocument.load(path);
+            PDFTextStripper sp = new PDFTextStripper();
+            richTextBox1.Text = (sp.getText(p));
+            str = (sp.getText(p));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            document.Print();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timPrint_Tick(object sender, EventArgs e)
+        {
+            //PDDocument p = PDDocument.load(path);
+            //PDFTextStripper sp = new PDFTextStripper();
+            //richTextBox1.Text = (sp.getText(p));
+            //str = (sp.getText(p));
+            //document.Print();
+        }
+    }
+}
